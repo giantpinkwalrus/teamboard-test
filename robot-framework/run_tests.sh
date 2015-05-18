3970a6b3d6880900284b5a9b21b1b62eff3e55b1
Paviaani <greenmonkey88@gmail.com>
2015-05-18 10:45:32 +0300

    Add run tests script

---
+++ robot-framework/run_tests.sh
@@ -0,0 +1,13 @@
+cd ContriboardTesting/
+pybot RegTestUsers.txt
+pybot Invalid_Login_Test.txt
+pybot New_User_Test.txt
+pybot Old\ User\ Test.txt
+cd .. && cd ContriboardTestScenarios/
+pybot RegisterUsers.txt
+pybot Scenario1.rst
+pybot Scenario2.rst
+pybot Scenario3.rst
+pybot Scenario4.rst
+pybot Scenario5.rst
+killall firefox
